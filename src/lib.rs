849862e50d4528f3c91f8efb285af5bfeeaa74e2
ostnam <tsougaevm@gmail.com>
2024-07-20 15:08:27 +0200

    Optimized comparison to other string types, doc comments + code structure.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 use std::alloc::{handle_alloc_error, Layout};
 use std::borrow::{Borrow, Cow};
 use std::ops::Deref;
-use std::ptr::null;
 use std::str::FromStr;
 
-/// The maximum number of chars a GermanStr can contain before requiring a heap allocation.
+/// The maximum number of chars a GermanStr can contain before requiring a heap
+/// allocation.
 pub const MAX_INLINE_BYTES: usize = 12;
 
 /// The absolute maximum number of chars a GermanStr can hold.
@@ -29,8 +29,8 @@ pub struct GermanStr {
     /// because UTF-8 strings are ordered byte-wise.
     prefix: [u8; 4],
 
-    /// If the string is longer than 12 bytes, is an owning, unique pointer to the
-    /// chars on the heap.
+    /// If the string is longer than 12 bytes, is an owning, unique pointer to
+    /// the chars on the heap.
     /// Otherwise, is an `[u8; 8]`, with extra bytes set to 0.
     /// The prefix is also included on the heap.
     last8: Last8,
@@ -47,75 +47,9 @@ union Last8 {
 
 #[derive(Debug, Clone, Copy)]
 pub enum InitError {
-    /// `GermanStr`s use an u32 to store their length, hence they can't contain more than 2^32 bytes (~4GB).
+    /// `GermanStr`s use an u32 to store their length, hence they can't contain
+    /// more than 2^32 bytes (~4GB).
     TooLong,
-}
-
-// Safety: According to the rustonomicon, "something can safely be Send unless it shares mutable
-// state with something else without enforcing exclusive access to it."
-// The `ptr` is never shared between `GermanStr`, so there's no shared mutable state.
-unsafe impl Send for GermanStr {}
-
-// Safety: Again, according to the rustonomicon, there's no issue here since GermanStr are
-// immutable.
-unsafe impl Sync for GermanStr {}
-
-impl Drop for GermanStr {
-    #[inline]
-    fn drop(&mut self) {
-        if let Some(ptr) = self.get_heap_ptr() {
-            let ptr = ptr.cast_mut();
-            unsafe {
-                // Safety: this call can only fail if self.len is too long.
-                // We can only create a long `GermanStr` using GermanStr::new: if `self.len`
-                // was too long, we'd get an error when we try to create the GermanStr.
-                let layout = Layout::array::<u8>(self.len as usize).unwrap_unchecked();
-                std::alloc::dealloc(ptr, layout);
-            }
-        }
-        // In the case where len <= MAX_INLINE_BYTES, no heap data is owned and
-        // no deallocation is needed.
-    }
-}
-
-impl Clone for GermanStr {
-    #[inline]
-    fn clone(&self) -> Self {
-        if let Some(self_ptr) = self.get_heap_ptr() {
-            let mut new = GermanStr {
-                prefix: self.prefix,
-                len: self.len,
-                last8: Last8 { ptr: null() },
-            };
-            let (ptr, layout) = unsafe {
-                // Safety: If len was too high for a valid layout, we couldn't
-                // have made self in the first place.
-                let layout = Layout::array::<u8>(self.len()).unwrap_unchecked();
-
-                // Safety: layout is not zero-sized, otherwise we would store the string inplace.
-                let ptr = std::alloc::alloc(layout);
-                (ptr, layout)
-            };
-            if ptr.is_null() {
-                handle_alloc_error(layout);
-            }
-            unsafe {
-                // Safety:
-                //   1. Both pointers are valid.
-                //   2. *_ u8 is always aligned.
-                //   3. The 2 regions can't overlap since they belong to different objects.
-                std::ptr::copy_nonoverlapping(self_ptr, ptr, self.len());
-            }
-            new.last8 = Last8 { ptr };
-            new
-        } else {
-            GermanStr {
-                len: self.len,
-                prefix: self.prefix,
-                last8: self.last8,
-            }
-        }
-    }
 }
 
 impl GermanStr {
@@ -140,17 +74,13 @@ impl GermanStr {
             handle_alloc_error(layout);
         }
         unsafe {
-                // Safety:
-                //   1. We assume src is a valid object.
-                //   2. ptr is valid: it was checked for null and allocated
-                //      for src.len() bytes.
-                //   3. *_ u8 is always aligned.
-                //   4. The 2 regions can't overlap since they belong to different objects.
-            std::ptr::copy_nonoverlapping(
-                src.as_bytes().as_ptr(),
-                ptr,
-                src.len(),
-            );
+            // Safety:
+            //   1. We assume src is a valid object.
+            //   2. ptr is valid: it was checked for null and allocated
+            //      for src.len() bytes.
+            //   3. *_ u8 is always aligned.
+            //   4. The 2 regions can't overlap since they belong to different objects.
+            std::ptr::copy_nonoverlapping(src.as_bytes().as_ptr(), ptr, src.len());
         }
         Ok(GermanStr {
             len: src.len() as u32,
@@ -189,6 +119,7 @@ impl GermanStr {
     }
 
     #[inline]
+    /// Safe accessor for last8.ptr.
     fn get_heap_ptr(&self) -> Option<*const u8> {
         if self.len as usize > MAX_INLINE_BYTES {
             Some(unsafe {
@@ -210,13 +141,19 @@ impl GermanStr {
     }
 
     #[inline]
-    /// Returns a slice containing every byte of a `GermanStr`, except the
-    /// first 4.
+    /// Returns a slice containing every byte of a `GermanStr`, except the first 4.
     pub fn suffix_bytes_slice(&self) -> &[u8] {
         let suffix_len = self.len().saturating_sub(4) as usize;
         if self.len as usize > MAX_INLINE_BYTES {
             unsafe {
                 let ptr = self.last8.ptr;
+
+                // Safety:
+                // 1. The data is part of a single object.
+                // 2. Pointer is checked for null at alloc.
+                // 3. ptr has the correct offset for the length
+                // 4. Heap values are properly initialized.
+                // 5. Values in the slice are never mutated.
                 std::slice::from_raw_parts(ptr.add(4), suffix_len)
             }
         } else {
@@ -252,6 +189,71 @@ impl GermanStr {
     }
 }
 
+impl Clone for GermanStr {
+    #[inline]
+    fn clone(&self) -> Self {
+        if let Some(self_ptr) = self.get_heap_ptr() {
+            let (ptr, layout) = unsafe {
+                // Safety: If len was too high for this layout, we couldn't
+                // have made self in the first place.
+                let layout = Layout::array::<u8>(self.len()).unwrap_unchecked();
+
+                // Safety: layout is not zero-sized, otherwise we would store the string inplace.
+                let ptr = std::alloc::alloc(layout);
+                (ptr, layout)
+            };
+            if ptr.is_null() {
+                handle_alloc_error(layout);
+            }
+            unsafe {
+                // Safety:
+                //   1. Both pointers are valid.
+                //   2. *_ u8 is always aligned.
+                //   3. The 2 regions can't overlap since they belong to different objects.
+                std::ptr::copy_nonoverlapping(self_ptr, ptr, self.len());
+            }
+            GermanStr {
+                prefix: self.prefix,
+                len: self.len,
+                last8: Last8 { ptr },
+            }
+        } else {
+            GermanStr {
+                len: self.len,
+                prefix: self.prefix,
+                last8: self.last8,
+            }
+        }
+    }
+}
+
+impl Drop for GermanStr {
+    #[inline]
+    fn drop(&mut self) {
+        if let Some(ptr) = self.get_heap_ptr() {
+            let ptr = ptr.cast_mut();
+            unsafe {
+                // Safety: this call can only fail if self.len is too long.
+                // We can only create a long `GermanStr` using GermanStr::new: if `self.len`
+                // was too long, we'd get an error when we try to create the GermanStr.
+                let layout = Layout::array::<u8>(self.len as usize).unwrap_unchecked();
+                std::alloc::dealloc(ptr, layout);
+            }
+        }
+        // In the case where len <= MAX_INLINE_BYTES, no heap data is owned and
+        // no deallocation is needed.
+    }
+}
+
+// Safety: According to the rustonomicon, "something can safely be Send unless it shares mutable
+// state with something else without enforcing exclusive access to it."
+// The `ptr` is never shared between `GermanStr`, so there's no shared mutable state.
+unsafe impl Send for GermanStr {}
+
+// Safety: Again, according to the rustonomicon, there's no issue here since GermanStr are
+// immutable.
+unsafe impl Sync for GermanStr {}
+
 impl Deref for GermanStr {
     type Target = str;
 
@@ -280,6 +282,7 @@ impl PartialEq<GermanStr> for GermanStr {
         self.prefix == other.prefix && self.suffix_bytes_slice() == other.suffix_bytes_slice()
     }
 }
+impl Eq for GermanStr {}
 
 impl Ord for GermanStr {
     #[inline]
@@ -288,73 +291,6 @@ impl Ord for GermanStr {
             .then_with(
                 || self.suffix_bytes_slice().cmp(other.suffix_bytes_slice())
             )
-    }
-}
-
-
-impl Eq for GermanStr {}
-
-impl PartialEq<str> for GermanStr {
-    #[inline(always)]
-    fn eq(&self, other: &str) -> bool {
-        self.as_str() == other
-    }
-}
-
-impl PartialEq<GermanStr> for str {
-    #[inline(always)]
-    fn eq(&self, other: &GermanStr) -> bool {
-        other == self
-    }
-}
-
-impl<'a> PartialEq<&'a str> for GermanStr {
-    #[inline(always)]
-    fn eq(&self, other: &&'a str) -> bool {
-        self == *other
-    }
-}
-
-impl<'a> PartialEq<GermanStr> for &'a str {
-    #[inline(always)]
-    fn eq(&self, other: &GermanStr) -> bool {
-        *self == other
-    }
-}
-
-impl PartialEq<String> for GermanStr {
-    #[inline(always)]
-    fn eq(&self, other: &String) -> bool {
-        other == self
-    }
-}
-
-impl PartialEq<GermanStr> for String {
-    #[inline(always)]
-    fn eq(&self, other: &GermanStr) -> bool {
-        other.prefix == str_prefix::<&String>(self)
-            && other.suffix_bytes_slice() == str_suffix::<&String>(self)
-    }
-}
-
-impl<'a> PartialEq<&'a String> for GermanStr {
-    #[inline(always)]
-    fn eq(&self, other: &&'a String) -> bool {
-        self == *other
-    }
-}
-
-impl<'a> PartialEq<GermanStr> for &'a String {
-    #[inline(always)]
-    fn eq(&self, other: &GermanStr) -> bool {
-        *self == other
-    }
-}
-
-impl PartialOrd for GermanStr {
-    #[inline(always)]
-    fn partial_cmp(&self, other: &GermanStr) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
     }
 }
 
@@ -369,12 +305,29 @@ impl Default for GermanStr {
     }
 }
 
-impl std::hash::Hash for GermanStr {
-    #[inline(always)]
-    fn hash<H: std::hash::Hasher>(&self, hasher: &mut H) {
-        self.as_str().hash(hasher);
+impl FromStr for GermanStr {
+    type Err = InitError;
+
+    #[inline]
+    fn from_str(s: &str) -> Result<GermanStr, Self::Err> {
+        GermanStr::new(s)
     }
 }
+
+impl Borrow<str> for GermanStr {
+    #[inline(always)]
+    fn borrow(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl std::fmt::Debug for GermanStr {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Debug::fmt(self.as_str(), f)
+    }
+}
+
 
 impl std::fmt::Display for GermanStr {
     #[inline]
@@ -383,14 +336,84 @@ impl std::fmt::Display for GermanStr {
     }
 }
 
-/*
-impl iter::FromIterator<char> for GermanStr {
-    fn from_iter<I: iter::IntoIterator<Item = char>>(iter: I) -> GermanStr {
-        let iter = iter.into_iter();
-        Self::from_char_iter(iter)
+impl std::hash::Hash for GermanStr {
+    #[inline(always)]
+    fn hash<H: std::hash::Hasher>(&self, hasher: &mut H) {
+        self.as_str().hash(hasher);
     }
 }
-*/
+
+
+impl PartialOrd for GermanStr {
+    #[inline(always)]
+    fn partial_cmp(&self, other: &GermanStr) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq<str> for GermanStr {
+    #[inline(always)]
+    fn eq(&self, other: &str) -> bool {
+        self.prefix == str_prefix::<&str>(other) &&
+        self.suffix_bytes_slice() == str_suffix::<&str>(&other)
+    }
+}
+
+impl PartialEq<GermanStr> for str {
+    #[inline(always)]
+    fn eq(&self, other: &GermanStr) -> bool {
+        other.prefix == str_prefix::<&str>(self) &&
+        other.suffix_bytes_slice() == str_suffix::<&str>(&self)
+    }
+}
+
+impl<'a> PartialEq<&'a str> for GermanStr {
+    #[inline(always)]
+    fn eq(&self, other: &&'a str) -> bool {
+        self.prefix == str_prefix::<&str>(other) &&
+        self.suffix_bytes_slice() == str_suffix::<&str>(&other)
+    }
+}
+
+impl<'a> PartialEq<GermanStr> for &'a str {
+    #[inline(always)]
+    fn eq(&self, other: &GermanStr) -> bool {
+        other.prefix == str_prefix::<&str>(self) &&
+        other.suffix_bytes_slice() == str_suffix::<&str>(&self)
+    }
+}
+
+impl PartialEq<String> for GermanStr {
+    #[inline(always)]
+    fn eq(&self, other: &String) -> bool {
+        self.prefix == str_prefix::<&str>(other) &&
+        self.suffix_bytes_slice() == str_suffix::<&str>(&other)
+    }
+}
+
+impl PartialEq<GermanStr> for String {
+    #[inline(always)]
+    fn eq(&self, other: &GermanStr) -> bool {
+        other.prefix == str_prefix::<&str>(self) &&
+        other.suffix_bytes_slice() == str_suffix::<&str>(&self)
+    }
+}
+
+impl<'a> PartialEq<&'a String> for GermanStr {
+    #[inline(always)]
+    fn eq(&self, other: &&'a String) -> bool {
+        self.prefix == str_prefix::<&str>(other) &&
+        self.suffix_bytes_slice() == str_suffix::<&str>(&other)
+    }
+}
+
+impl<'a> PartialEq<GermanStr> for &'a String {
+    #[inline(always)]
+    fn eq(&self, other: &GermanStr) -> bool {
+        other.prefix == str_prefix::<&str>(self) &&
+        other.suffix_bytes_slice() == str_suffix::<&str>(&self)
+    }
+}
 
 impl TryFrom<&str> for GermanStr {
     type Error = InitError;
@@ -437,18 +460,6 @@ impl<'a> TryFrom<Cow<'a, str>> for GermanStr {
     }
 }
 
-/*
-impl From<GermanStr> for Arc<str> {
-    #[inline(always)]
-    fn from(text: GermanStr) -> Self {
-        match text.0 {
-            Repr::Heap(data) => data,
-            _ => text.as_str().into(),
-        }
-    }
-}
-*/
-
 impl From<GermanStr> for String {
     #[inline(always)]
     fn from(text: GermanStr) -> Self {
@@ -456,35 +467,9 @@ impl From<GermanStr> for String {
     }
 }
 
-impl Borrow<str> for GermanStr {
-    #[inline(always)]
-    fn borrow(&self) -> &str {
-        self.as_str()
-    }
-}
-
-impl FromStr for GermanStr {
-    type Err = InitError;
-
-    #[inline]
-    fn from_str(s: &str) -> Result<GermanStr, Self::Err> {
-        GermanStr::new(s)
-    }
-}
-
-impl std::fmt::Debug for GermanStr {
-    #[inline]
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        std::fmt::Debug::fmt(self.as_str(), f)
-    }
-}
-
-#[inline(always)]
-pub fn str_suffix<T>(src: &impl AsRef<str>) -> &[u8] {
-    src.as_ref().as_bytes().get(4..).unwrap_or_default()
-}
-
-#[inline(always)]
+#[inline]
+/// Returns the first 4 bytes of a string.
+/// If the string has less than 4 bytes, extra bytes are set to 0.
 pub fn str_prefix<T>(src: impl AsRef<str>) -> [u8; 4] {
     let src = src.as_ref().as_bytes();
     let mut bytes = [0; 4];
@@ -492,4 +477,10 @@ pub fn str_prefix<T>(src: impl AsRef<str>) -> [u8; 4] {
         bytes[i] = src[i];
     }
     bytes
+}
+
+#[inline]
+/// Returns a slice to every byte of the string, skipping the first 4.
+pub fn str_suffix<T>(src: &impl AsRef<str>) -> &[u8] {
+    src.as_ref().as_bytes().get(4..).unwrap_or_default()
 }
