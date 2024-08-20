415414dcaaa5687f656f28d9834d6d635eab3e7e
ostnam <tsougaevm@gmail.com>
2024-08-20 12:39:13 +0200

    Ord benchmarks

--- benches/bench_vec_sorting.rs
+++ benches/bench_vec_sorting.rs
@@ -1,8 +1,109 @@
 use german_str::GermanStr;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use rand::Rng as _;
-use rand::distributions::{Alphanumeric, Uniform};
+use rand::distributions::Alphanumeric;
+use smol_str::SmolStr;
+
+fn comparison_benches(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Comparing strings.");
+    group.bench_function(
+        "german strings, 4 chars",
+        |b| b.iter_batched_ref(
+            || (GermanStr::new(gen_random_string(4)).unwrap(), GermanStr::new(gen_random_string(4)).unwrap()),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "smolstr, 4 chars",
+        |b| b.iter_batched_ref(
+            || (SmolStr::new(gen_random_string(4)), SmolStr::new(gen_random_string(4))),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "String, 4 chars",
+        |b| b.iter_batched_ref(
+            || (gen_random_string(4), gen_random_string(4)),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "german strings, 10 chars",
+        |b| b.iter_batched_ref(
+            || (GermanStr::new(gen_random_string(10)).unwrap(), GermanStr::new(gen_random_string(10)).unwrap()),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "smolstr, 10 chars",
+        |b| b.iter_batched_ref(
+            || (SmolStr::new(gen_random_string(10)), SmolStr::new(gen_random_string(10))),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "String, 10 chars",
+        |b| b.iter_batched_ref(
+            || (gen_random_string(10), gen_random_string(10)),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "german strings, 20 chars",
+        |b| b.iter_batched_ref(
+            || (GermanStr::new(gen_random_string(20)).unwrap(), GermanStr::new(gen_random_string(20)).unwrap()),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "smolstr, 20 chars",
+        |b| b.iter_batched_ref(
+            || (SmolStr::new(gen_random_string(20)), SmolStr::new(gen_random_string(20))),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "String, 20 chars",
+        |b| b.iter_batched_ref(
+            || (gen_random_string(20), gen_random_string(20)),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "german strings, 50 chars",
+        |b| b.iter_batched_ref(
+            || (GermanStr::new(gen_random_string(50)).unwrap(), GermanStr::new(gen_random_string(50)).unwrap()),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "smolstr, 50 chars",
+        |b| b.iter_batched_ref(
+            || (SmolStr::new(gen_random_string(50)), SmolStr::new(gen_random_string(50))),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+    group.bench_function(
+        "String, 50 chars",
+        |b| b.iter_batched_ref(
+            || (gen_random_string(50), gen_random_string(50)),
+            |(a, b)| a.cmp(&b),
+            criterion::BatchSize::SmallInput,
+        )
+    );
+}
 
 fn gen_random_string(len: usize) -> String {
     let mut char_gen = rand::thread_rng().sample_iter(Alphanumeric);
@@ -13,95 +114,5 @@ fn gen_random_string(len: usize) -> String {
     String::from_utf8(vec).unwrap()
 }
 
-fn bench_mixed(c: &mut Criterion) {
-    let mut string_lengths = rand::thread_rng().sample_iter(Uniform::new(2, 50));
-    let mut strings = Vec::new();
-    for _ in 0..1000 {
-        let len = string_lengths.next().unwrap();
-        strings.push(gen_random_string(len));
-    }
-    let german_strings: Vec<_> = strings.iter()
-        .map(|s| GermanStr::new(s).unwrap())
-        .collect();
-    c.bench_function(
-        "sort regular strings",
-        |b| b.iter_batched(
-            || strings.clone(),
-            |mut data| black_box(data.sort()),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-    c.bench_function(
-        "sort german strings",
-        |b| b.iter_batched(
-            || german_strings.clone(),
-            |mut data| black_box(data.sort()),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-}
-
-fn bench_short(c: &mut Criterion) {
-    let mut char_gen = rand::thread_rng().sample_iter(Alphanumeric);
-    let mut string_lengths = rand::thread_rng().sample_iter(Uniform::new(0, 12));
-    let mut strings = Vec::new();
-    for _ in 0..1000 {
-        let len = string_lengths.next().unwrap();
-        let mut vec = Vec::new();
-        for _ in 0..len {
-            vec.push(char_gen.next().unwrap());
-        }
-        let string = String::from_utf8(vec).unwrap();
-        strings.push(string);
-    }
-    let german_strings: Vec<_> = strings.iter()
-        .map(|s| GermanStr::new(s).unwrap())
-        .collect();
-    c.bench_function(
-        "sort short regular strings",
-        |b| b.iter_batched(
-            || strings.clone(),
-            |mut data| black_box(data.sort()),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-    c.bench_function(
-        "sort short german strings",
-        |b| b.iter_batched(
-            || german_strings.clone(),
-            |mut data| black_box(data.sort()),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-}
-
-
-fn bench_compare_to_string(c: &mut Criterion) {
-    c.bench_function(
-        "compare two regular strings",
-        |b| b.iter_batched(
-            || (gen_random_string(10), gen_random_string(10)),
-            |(a, b)| black_box(a == b),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-    c.bench_function(
-        "compare regular string to german string",
-        |b| b.iter_batched(
-            || (gen_random_string(10), GermanStr::new(gen_random_string(10)).unwrap()),
-            |(a, b)| black_box(a == b),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-    c.bench_function(
-        "compare two german strings",
-        |b| b.iter_batched(
-            || (GermanStr::new(gen_random_string(10)).unwrap(), GermanStr::new(gen_random_string(10)).unwrap()),
-            |(a, b)| black_box(a == b),
-            criterion::BatchSize::SmallInput,
-        )
-    );
-}
-
-criterion_group!(benches, bench_mixed, bench_short, bench_compare_to_string);
+criterion_group!(benches, comparison_benches);
 criterion_main!(benches);
